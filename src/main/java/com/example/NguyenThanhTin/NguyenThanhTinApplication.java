0a75715a2f6942a894547344ff3f31af4023c42d
tin2282002@gmail.com <tin2282002@gmail.com>
2025-02-24 10:32:16 +0700

    commit

---
+++ src/main/java/com/example/NguyenThanhTin/NguyenThanhTinApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenThanhTin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenThanhTinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenThanhTinApplication.class, args);
+	}
+
+}
