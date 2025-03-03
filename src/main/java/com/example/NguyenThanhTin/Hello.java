96a48007bef875e38bda201071f3f727c167aa65
tin2282002@gmail.com <tin2282002@gmail.com>
2025-03-03 11:22:50 +0700

    buoi2

---
+++ src/main/java/com/example/NguyenThanhTin/Hello.java
@@ -0,0 +1,37 @@
+package com.example.NguyenThanhTin;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController 	
+@RequestMapping("/products")
+public class Hello {
+    @GetMapping
+    String GetProduct(){
+        return "bạn đã lấy sản phẩm ";
+    }
+    @PostMapping
+    public String postProduct() {
+
+        return "bạn đã thêm sản phẩm ";
+    }
+    @PutMapping("/{id}")
+     public String putProduct(@PathVariable int id) {
+        
+        return "bạn đã cập nhật sản phẩm  id: "+id;
+     }
+     @DeleteMapping("/{id}")
+     public String delProduct(@PathVariable int id) {
+        
+        return "bạn đã xóa sản phẩm id: "+id;
+    }
+
+}
--- src/main/java/com/example/NguyenThanhTin/NguyenThanhTinApplication.java
+++ src/main/java/com/example/NguyenThanhTin/NguyenThanhTinApplication.java
@@ -2,10 +2,17 @@ package com.example.NguyenThanhTin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NguyenThanhTinApplication {
 
+	// @RequestMapping("/")
+	// String home() {
+	// 	System.out.println("Hello");
+	// 	return "Hello World!";
+	// }
 	public static void main(String[] args) {
 		SpringApplication.run(NguyenThanhTinApplication.class, args);
 	}
